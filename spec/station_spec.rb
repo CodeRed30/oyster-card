9d81f18942ae18df7856795759f17a7a3a4cb6ab
Miranda Wilson <mirandascwilson@gmail.com>
2021-01-13 15:33:07 +0000

    fixed formatting and DRY'd

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,11 +1,8 @@
 require 'station'
 
 describe Station do
-# let(:station){ { name: "Bank", zone: 1 } }
-subject(:station) { described_class.new("Bank", 1) }
+  subject(:station) { described_class.new("Bank", 1) }
   context 'initalize' do
-    it { is_expected.to respond_to(:name) }
-
     it 'with the name "Bank"' do
       expect(subject.name).to eq("Bank")
     end

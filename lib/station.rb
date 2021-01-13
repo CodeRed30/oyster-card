b0a00a0d0ce9827170af0fe27d13ab421d93c3bb
Miranda Wilson <mirandascwilson@gmail.com>
2021-01-13 15:31:30 +0000

    Step13 - added zone instance variable for station

--- lib/station.rb
+++ lib/station.rb
@@ -1,6 +1,7 @@
 class Station
-  attr_reader :name
-  def initialize(name = "Bank")
+  attr_reader :name, :zone
+  def initialize(name, zone)
     @name = name
+    @zone = zone
   end
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,12 +1,17 @@
 require 'station'
 
 describe Station do
-let(:station){ { name: "Bank" } }
+# let(:station){ { name: "Bank", zone: 1 } }
+subject(:station) { described_class.new("Bank", 1) }
   context 'initalize' do
     it { is_expected.to respond_to(:name) }
 
     it 'with the name "Bank"' do
-      expect(subject.name).to eq(station[:name])
+      expect(subject.name).to eq("Bank")
+    end
+
+    it "has correct zone" do
+      expect(subject.zone).to eq(1)
     end
   end
 end
